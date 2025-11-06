5eea772e818ecb8473a71cfce9e8b37805cc9f5c
Lucas Luján <lucasnlujan@gmail.com>
2025-11-06 00:01:48 +0000

    Commit 2

--- src/login.js
+++ src/login.js
@@ -2,22 +2,48 @@ const URL = import.meta.env.VITE_API_URL
 const username = document.getElementById("username")
 const password = document.getElementById("password")
 const boton = document.getElementById("obtener")
+const ATO = localStorage.getItem("ATO")
+
+if (ATO) {
+    window.location.href = '/'
+
+}
+
 
 boton.onclick = () => {
-    const body = JSON.stringify({ username: username.value, password: password.value })
-    fetch(`${URL}/auth`, {
+    const body = JSON.stringify({ username: username.value, password: password.value }) //información que vamos a mandar. El stringify es para comunicar al backend en un formato JSON y para no cometer conflictos (el backend puede que este en otro lenguaje) se manda en string.
+    fetch(`${URL}/auth`, { //el auth es el nombre del recurso. Nos proporciona recursos sobre login. /task sería de tareas.
         method: 'POST',
         body,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json" //con el content type se le avisa al servidor que tipo de contenido le estamos mandando, le solicitamos que entienda mi mensaje en JSON.
         }
     })
-        .then((response) => response.json())
+        .then((response) => response.json()) //el then es el proceso inverso al stringify, convierto el formato JSON en un objeto de javascript, como es una promesa puedo ir haciendolo por partes, el then que le sigue tendrá una función "return" -> de respuesta, si en algún momento irá al catch.
         .then((json) => {
             console.log(json)
-            localStorage.setItem("ATO", json.ATO)
-        });
+
+            if (json.ATO) {
+
+                localStorage.setItem("ATO", json.ATO) //se guarda si el ATO fue recibido. (pero se guarda sólo de forma local -> el localStorage permanece si apago la computadora o navego en otras webs)
+
+            }
+
+        }).catch((err) => {  //El catch agarra el error y lo maneja de alguna forma. se realizó una función anónima (()) =>
+
+            const { message } = err
+
+            console.log(message)
+
+
+        }
+
+        )
 }
+
+// export function hola () {
+// console.log("hola")
+// }
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-const URL = import.meta.env.API_URL
+// import {hola} from "./login.js"
 
+const URL = import.meta.env.VITE_API_URL
+// hola()
 const obtener = document.getElementById("obtener")
 const tareas = document.getElementById("tareas")
+
+const ATO = localStorage.getItem("ATO")
+
+
+
+
 
 
 function crearTarjeta(title, completed) {
@@ -11,22 +19,40 @@ function crearTarjeta(title, completed) {
   return div
 }
 
-function obtenerTareasPaginadas (page=0,take=5) {
-  fetch(`${URL}/tasks?page=${page}&take=${take}`)
-    .then((response) => response.json())
-    .then((json) => {
+function obtenerTareasPaginadas(page = 0, take = 5) {
+  fetch(`${URL}/tasks?page=${page}&take=${take}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": ATO //se utiliza la variable "ATO"
+    }
+  })
+    .then((a) => a.json())
+    .then(json => {
+      console.log(json)
       json.data.forEach(element => {
         const card = crearTarjeta(element.title, element.completed)
         tareas.appendChild(card)
       });
-    const cantPages= Math.ceil(json.total_result/take)
+      const cantPages = Math.ceil(json.total_result / take)
     });
 }
 
-function crearComponentePaginado (cantPages, page) {
-  for (let i=0; i<cantPages; i++){
-    crearboton (i)
+function crearComponentePaginado(cantPages, page) {
+  for (let i = 0; i < cantPages; i++) {
+    crearboton(i)
   }
 }
 
-obtenerTareasPaginadas()
+if (!ATO) { // Si el usuario no se encuentra logueado la página se mantendrá en "login" para que inicie sesión.
+  // el "!" se refiere a una negación, por eso !ATO (si no hay ATO)
+  window.location.href = "login"
+
+} else {
+
+  obtenerTareasPaginadas()
+
+}
+
+
+
