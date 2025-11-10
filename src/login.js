fba8667c4a33394dceecdb93b63f2895b45d1b66
Lucas Luján <lucasnlujan@gmail.com>
2025-11-10 23:48:58 +0000

    Commit 3 se comentaron líneas de código.

--- src/login.js
+++ src/login.js
@@ -1,19 +1,28 @@
 const URL = import.meta.env.VITE_API_URL
 const username = document.getElementById("username")
 const password = document.getElementById("password")
+// username y password son un input.
 const boton = document.getElementById("obtener")
+// "obtener" es un botón.
 const ATO = localStorage.getItem("ATO")
 
 if (ATO) {
     window.location.href = '/'
 
 }
+// lo que hace este if es verificar si tenemos ATO o no. '/' -> refiere a que me manda la pagina mas alta. Por ejemplo, te logueas y te manda a location de la pagina de inicio (index)
+// en este if también podríamos agregar cuanto tiempo de validez tiene el token. Si el token existe y es válido redirijirá al usuario a la pagina que corresponda, en este caso '/'
 
 
 boton.onclick = () => {
     const body = JSON.stringify({ username: username.value, password: password.value }) //información que vamos a mandar. El stringify es para comunicar al backend en un formato JSON y para no cometer conflictos (el backend puede que este en otro lenguaje) se manda en string.
     fetch(`${URL}/auth`, { //el auth es el nombre del recurso. Nos proporciona recursos sobre login. /task sería de tareas.
-        method: 'POST',
+        method: 'POST', 
+        // get -> para obtener 
+        // post -> para crear
+        // patch -> para editar / objeto parcial.
+        // put -> para editar / todo el objeto.
+        // delete -> en vez de borrar algo del registro lo marco como borrado -> se utiliza para que quede el dato exclusivamente para el backend y no para el front osea no para el usuario. Suele utilizarse para datos de importancia ej -> bancos.
         body,
         headers: {
             "Content-Type": "application/json" //con el content type se le avisa al servidor que tipo de contenido le estamos mandando, le solicitamos que entienda mi mensaje en JSON.
@@ -31,7 +40,8 @@ boton.onclick = () => {
 
         }).catch((err) => {  //El catch agarra el error y lo maneja de alguna forma. se realizó una función anónima (()) =>
 
-            const { message } = err
+            const { message } = err 
+            // acá el catch informa al usuario de que falló algo y qué fallo.
 
             console.log(message)
 
--- src/main.js
+++ src/main.js
@@ -1,38 +1,50 @@
 // import {hola} from "./login.js"
 
-const URL = import.meta.env.VITE_API_URL
+const URL = import.meta.env.VITE_API_URL 
 // hola()
 const obtener = document.getElementById("obtener")
 const tareas = document.getElementById("tareas")
-
 const ATO = localStorage.getItem("ATO")
-
-
-
-
-
+// autorización es que el sistema pueda determinar que soy yo, y el permiso es que el sistema sabiendo que soy yo luego determina si tengo permisos para algo o no. El ATO sirve para que el sistema pueda autorizarme, osea que me reconozca.
+// el token puede estar guardado en el local-storage. Al abrir nuestro archivo es importante revisar si tenemos el token o no. Revisar si hay ATO o no. -> si no hay un ATO me redirige al /login.
 
 function crearTarjeta(title, completed) {
   const div = document.createElement("div")
   div.classList.add("card")
-  div.innerHTML = `<span>${title}</span> <span>${completed ? "SI" : "NO"}</span>`
+  div.innerHTML = `<span>${title}</span> <span class="completado">${completed ? "SI" : "NO"}</span>`
+  div.getElementsByClassName("completado")[0]
+  //el [0] -> es para acceder al primero de los elementos del array, el array es una lista ordenada, como esta ordenada yo puedo acceder a algun elemento que lo componen, se arranca con el número 0, y luego 1, 2, 3 etc. Depende del elemento que busco acceder de ese array.
+  const completado = div.getElementsByClassName("completado")[0]
+  completado.onclick = () => {
+    console.log(holapapu)
+  }
   return div
 }
 
 function obtenerTareasPaginadas(page = 0, take = 5) {
   fetch(`${URL}/tasks?page=${page}&take=${take}`, {
+  // el fetch tiene la URL que declaramos en una variable, (el fetch es una promesa) el task es el recurso que buscamos acceder osea "tareas"
     method: "GET",
+    // metodos: get, put, git, patch.
     headers: {
       "Content-Type": "application/json",
-      "Authorization": ATO //se utiliza la variable "ATO"
+      // le decimos a la computadora lo que le estamos mandando y el "type" sería el tipo ej json, jpg, etc.
+      "Authorization": ATO 
+      //se utiliza la variable "ATO" . Puede haber un ATO, una key, va algo secreto.
     }
   })
-    .then((a) => a.json())
+  // las líneas 31-40 se realiza una operación de la promesa con la finalidad de transformar el codificado json a javascript
+    .then((a) => a.json()) 
+    // acá sería la respuesta (retorno) de las lineas 31-40.
     .then(json => {
+      // lo que toma un then lo retorna en el siguiente then.
+      
       console.log(json)
       json.data.forEach(element => {
         const card = crearTarjeta(element.title, element.completed)
         tareas.appendChild(card)
+        // append -> agrega al final de los hijos.
+        // ejemplos distintos: 
       });
       const cantPages = Math.ceil(json.total_result / take)
     });
@@ -47,7 +59,7 @@ function crearComponentePaginado(cantPages, page) {
 if (!ATO) { // Si el usuario no se encuentra logueado la página se mantendrá en "login" para que inicie sesión.
   // el "!" se refiere a una negación, por eso !ATO (si no hay ATO)
   window.location.href = "login"
-
+  // el window.location.href sirve para la redirección. el .href me redirije a la página /login en este caso. Rutas relativas es poner ej: ./login 
 } else {
 
   obtenerTareasPaginadas()
@@ -55,4 +67,4 @@ if (!ATO) { // Si el usuario no se encuentra logueado la página se mantendrá e
 }
 
 
-
+// comandos de consola: ls -> para listar carpetas. cd -> abrir una carpeta. por ej -> se utiliza para redireccionar archivos, imagenes, etc. Sirve para hacer scripts. Hay comandos también para buscar textos, revisar peso, revisar últimas modificaciones.
